f2eb1818afe0b77f0461fedab155b55a2b9413c8
Sandro <sandro.soria@proton.me>
2026-05-01 14:07:24 +0200

    remove generated target files and cache directories

--- target/debug/deps/NL_Client-27c8abd4ed58de5f.d
+++
@@ -1,5 +0,0 @@
-/Users/sandromacbook/Documents/GitHub/NodeLink-Project/NL-Client/target/debug/deps/NL_Client-27c8abd4ed58de5f.d: src/main.rs
-
-/Users/sandromacbook/Documents/GitHub/NodeLink-Project/NL-Client/target/debug/deps/libNL_Client-27c8abd4ed58de5f.rmeta: src/main.rs
-
-src/main.rs:
--- target/debug/deps/NL_Client-9798b72eb6344169.d
+++
@@ -1,5 +0,0 @@
-/Users/sandromacbook/Documents/GitHub/NodeLink-Project/NL-Client/target/debug/deps/NL_Client-9798b72eb6344169.d: src/main.rs
-
-/Users/sandromacbook/Documents/GitHub/NodeLink-Project/NL-Client/target/debug/deps/libNL_Client-9798b72eb6344169.rmeta: src/main.rs
-
-src/main.rs:
